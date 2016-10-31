e570225536229803cd80ca2564527bab9b7ee0ff
Meck <yesmeck@gmail.com>
2016-10-31 20:05:28 +0800

    WIP

---
+++ src/columnManager.js
@@ -0,0 +1,118 @@
+export default class ColumnManager {
+  static getLeafColumns(columns) {
+    const leafColumns = [];
+    columns.forEach(column => {
+      if (!column.children) {
+        leafColumns.push(column);
+      } else {
+        leafColumns.push(...ColumnManager.getLeafColumns(column.children));
+      }
+    });
+    return leafColumns;
+  }
+
+  static getLeafColumnsCount(columns) {
+    return ColumnManager.getLeafColumns(columns).count;
+  }
+
+  constructor(columns) {
+    this.columns = columns;
+  }
+
+  cached = {}
+
+  cache(name, fn) {
+    if (name in this.cached) {
+      return this.cached[name];
+    }
+    this.cached[name] = fn();
+    return this.cached[name]
+  }
+
+  isAnyColumnsFixed() {
+    return this.cache('isAnyColumnsFixed', () => {
+      return this.columns.some(column => !!column.fixed);
+    });
+  }
+
+  isAnyColumnsLeftFixed() {
+    return this.cache('isAnyColumnsLeftFixed', () => {
+      return this.columns.some(
+        column => column.fixed === 'left' || column.fixed === true
+      );
+    });
+  }
+
+  isAnyColumnsRightFixed() {
+    return this.cache('isAnyColumnsRightFixed', () => {
+      return this.columns.some(
+        column => column.fixed === 'right'
+      );
+    })
+  }
+
+  leftColumns() {
+    return this.cache('leftColumns', () => {
+      return this.groupedColumns().filter(
+        column => column.fixed === 'left' || column.fixed === true
+      );
+    })
+  }
+
+  rightColumns() {
+    return this.cache('rightColumns', () => {
+      return this.groupedColumns().filter(
+        column => column.fixed === 'right'
+      );
+    });
+  }
+
+  // add appropriate rowspan and colspan to column
+  groupedColumns() {
+    return this.cache('groupedColumns', () => {
+      const _groupColumns = (columns, currentRow = 0, parentColumn = {}, rows = []) => {
+        // track how many rows we got
+        if (!~rows.indexOf(currentRow)) {
+          rows.push(currentRow);
+        }
+        const grouped = [];
+        const setRowSpan = column => {
+          const rowSpan = rows.length - currentRow;
+          if (column &&
+            !column.children && // parent columns are supposed to be one row
+            rowSpan > 1 &&
+            (!column.rowSpan || column.rowSpan < rowSpan)
+          ) {
+            column.rowSpan = rowSpan;
+          }
+        };
+        columns.forEach((column, index) => {
+          const newColumn = { ...column };
+          parentColumn.colSpan = parentColumn.colSpan || 0;
+          if (newColumn.children && newColumn.children.length > 0) {
+            newColumn.children = _groupColumns(newColumn.children, currentRow + 1, newColumn, rows);
+            parentColumn.colSpan = parentColumn.colSpan + newColumn.colSpan;
+          } else {
+            parentColumn.colSpan++;
+          }
+          // update rowspan to all previous columns
+          for (let i = 0; i < index; ++i) {
+            setRowSpan(grouped[i]);
+          }
+          // last column, update rowspan immediately
+          if (index + 1 === columns.length) {
+            setRowSpan(newColumn);
+          }
+          grouped.push(newColumn);
+        });
+        return grouped;
+      }
+      return _groupColumns(this.columns);
+    })
+  }
+
+  reset(columns) {
+    this.columns = columns;
+    this.cache = {};
+  }
+}
